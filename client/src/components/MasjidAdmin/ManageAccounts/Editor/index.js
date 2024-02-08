0a59c11564547d30427b47668f2f4a29c155b343
kapilrajreddyp <kapilrajreddy.patlola@labyrinthglobalsolutions.com>
2024-02-08 15:29:03 +0530

    modified

--- client/src/components/MasjidAdmin/ManageAccounts/Editor/index.js
+++ client/src/components/MasjidAdmin/ManageAccounts/Editor/index.js
@@ -2,54 +2,158 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { ImCancelCircle } from 'react-icons/im';
- 
- 
-const NewEditor = ({onClose}) => {
+import Cookies from 'js-cookie';
 
-  const handleCloseModel=()=>{
-    onClose()
-  }
+const NewEditor = ({ onClose }) => {
+  const [formData, setFormData] = useState({
+    name:'',
+    email: '',
+    phoneNumber: '',
+    roleid: parseInt('0'),
+    comment: ''
+  });
 
-    return(
-<>
-<div style={{display:"flex",justifyContent:"flex-end"}}>
-      <ImCancelCircle className= "handle-close-icon" onClick={handleCloseModel}/>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleCloseModel = () => {
+    onClose();
+  };
+
+  const token = Cookies.get("user");
+      
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch(`http://localhost:3009/api/v1/addadminstaff`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(formData)
+      });
+      if (response.ok) {
+        console.log('Data submitted successfully');
+        // Handle success, e.g., show a success message to the us
+
+        setFormData({
+          name:'',
+          email: '',
+          phoneNumber: '',
+          roleid: parseInt('0'),
+          comment: ''
+        })
+
+      } else {
+        console.error('Failed to submit data');
+        // Handle failure, e.g., show an error message to the user
+      }
+    } catch (error) {
+      console.error('Error occurred while submitting data:', error);
+      // Handle error, e.g., show an error message to the user
+    }
+  };
+
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <ImCancelCircle className="handle-close-icon" onClick={handleCloseModel} />
       </div>
-<div style={{margin: '70px'}}>
-<h1 className='neweditor-heading'>Add New Editor</h1>
-<div className="container">
-      <div className="row">
-        <div className="col-md-4">
-          <label htmlFor="inputEmail" className="form-label neweditor-label">Email:</label>
-          <input type="email" className="form-control" id="inputEmail" placeholder="Enter your email" />
-        </div>
- 
-        <div className="col-md-4">
-          <label htmlFor="inputPhoneNumber" className="form-label neweditor-label">Phone Number:</label>
-          <input type="tel" className="form-control" id="inputPhoneNumber" placeholder="Enter your phone number" />
-        </div>
- 
-        <div className="col-md-4">
-          <label htmlFor="selectRole" className="form-label neweditor-label">Role:</label>
-          <select className="form-control" id="selectRole">
-            <option value="admin" className='neweditor-label'>Admin Editor</option>
-            <option value="message" className='neweditor-label'>Message Editor</option>
-           
-            {/* Add more options as needed */}
-          </select>
-        </div>
-        <div className="col-md-12 mt-3">
-          <label htmlFor="commentBox" className="form-label">Comment:</label>
-          <textarea className="form-control" id="commentBox" rows="6" placeholder="Enter your comment"></textarea>
-        </div>
-        <div style={{display: "flex", flexDirection: "row-reverse" , margin: "30px 0px" }}>
-            <button className='btn btn-info' style={{color: "white"}}>Add Editor</button>
+      <div style={{ margin: '70px' }}>
+        <h1 className="neweditor-heading">Add New Editor</h1>
+        <div className="container">
+          <div className="row">
+          <div className="col-md-3">
+              <label htmlFor="inputEmail" className="form-label neweditor-label">
+                Name:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+               
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Enter your name"
+              />
+            </div>
+            <div className="col-md-3">
+              <label htmlFor="inputEmail" className="form-label neweditor-label">
+                Email:
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="inputEmail"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Enter your email"
+              />
+            </div>
+
+            <div className="col-md-3">
+              <label htmlFor="inputPhoneNumber" className="form-label neweditor-label">
+                Phone Number:
+              </label>
+              <input
+                type="tel"
+                className="form-control"
+                id="inputPhoneNumber"
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                placeholder="Enter your phone number"
+              />
+            </div>
+
+            <div className="col-md-3">
+              <label htmlFor="selectRole" className="form-label neweditor-label">
+                Role:
+              </label>
+              <select
+                className="form-control"
+                id="selectRole"
+                name="roleid"
+                value={formData.roleid}
+                onChange={handleChange}
+              >
+                <option value="0" className="neweditor-label">
+                  Admin Editor
+                </option>
+                <option value="1" className="neweditor-label">
+                  Message Editor
+                </option>
+                {/* Add more options as needed */}
+              </select>
+            </div>
+            <div className="col-md-12 mt-3">
+              <label htmlFor="commentBox" className="form-label">
+                Comment:
+              </label>
+              <textarea
+                className="form-control"
+                id="commentBox"
+                name="comment"
+                value={formData.comment}
+                onChange={handleChange}
+                rows="6"
+                placeholder="Enter your comment"
+              ></textarea>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'row-reverse', margin: '30px 0px' }}>
+              <button className="btn btn-info" style={{ color: 'white' }} onClick={handleSubmit}>
+                Add Editor
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-</div>
-</div>
-</>
-    )
-}
- 
-export default NewEditor
+    </>
+  );
+};
+
+export default NewEditor;
--- client/src/components/MasjidAdmin/ManageAccounts/index.js
+++ client/src/components/MasjidAdmin/ManageAccounts/index.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import {Link} from 'react-router-dom';
-
 import { Box, Modal } from "@mui/material";
 import NewEditor from './Editor';
 import EditButton from './EditButton';
@@ -13,116 +11,120 @@ const Manageaccounts = () => {
         { id: 1, email: 'user1@example.com', phone: '123-456-7890', createdAt: '2022-01-01', lastLoggedIn: '2022-01-15' },
         { id: 2, email: 'user2@example.com', phone: '987-654-3210', createdAt: '2022-01-05', lastLoggedIn: '2022-01-20' },
         // Add more user data as needed
-      ];
+    ];
 
-      const [isModalOpen, setModalOpen] = React.useState(false);
-      const [editModel,setEditModel] = useState(false)
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [editModel, setEditModel] = useState(false);
+    const [selectedUser, setSelectedUser] = useState(null);
 
-  const handleCloseModal = () => {
-    setModalOpen(false);
-    setEditModel(false)
-  };
+    const handleCloseModal = () => {
+        setModalOpen(false);
+        setEditModel(false);
+    };
 
-  const handleEditors=()=>{
-    setModalOpen(true)
-  }
+    const handleEditors = () => {
+        setModalOpen(true);
+    };
 
-  const handleEditButton=()=>{
-    setEditModel(true)
-  }
+    const handleEditButton = (user) => {
+        setSelectedUser(user);
+        setEditModel(true);
+    };
  
-return(
-<>
-<div style={{margin: '70px'}}>
-    <div style={{display: "flex", justifyContent: "space-between"}}>
-        <h1 className='manage-heading1'>ALL EDITORS</h1>
-        <button className='btn btn-info' style={{color: "white"}} onClick={handleEditors}>Add Editors</button>
-    </div>
-    <div className="container mt-5 manage-whole-container">
-     
-      <table className="table">
-        <thead>
-          <tr>
-            <th className='manage-th manage-heading'>ID</th>
-            <th className='manage-th manage-heading'>Email Address</th>
-            <th className='manage-th manage-heading'>Phone</th>
-            <th className='manage-th manage-heading'>Created At</th>
-            <th className='manage-th manage-heading'>Last Logged In</th>
-            <th className='manage-th manage-heading'>Actions</th>
-          </tr>
-        </thead>
-        <tbody className='table-manage-body'>
-          {users.map(user => (
-            <tr key={user.id}>
-              <td className='manage-para'>{user.id}</td>
-              <td className='manage-para'>{user.email}</td>
-              <td className='manage-para'>{user.phone}</td>
-              <td className='manage-para'>{user.createdAt}</td>
-              <td className='manage-para'>{user.lastLoggedIn}</td>
-              <td>
-                {/* Add your action buttons here */}
-                <button className="edit-button-manage" onClick={handleEditButton}>Edit</button>
-               
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-</div>
+    return (
+        <>
+            <div style={{margin: '70px'}}>
+                <div style={{display: "flex", justifyContent: "space-between"}}>
+                    <h1 className='manage-heading1'>ALL EDITORS</h1>
+                    <button className='btn btn-info' style={{color: "white"}} onClick={handleEditors}>Add Editors</button>
+                </div>
+                <div className="container mt-5 manage-whole-container">
+                
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th className='manage-th manage-heading'>ID</th>
+                                <th className='manage-th manage-heading'>Email Address</th>
+                                <th className='manage-th manage-heading'>Phone</th>
+                                <th className='manage-th manage-heading'>Created At</th>
+                                <th className='manage-th manage-heading'>Last Logged In</th>
+                                <th className='manage-th manage-heading'>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody className='table-manage-body'>
+                            {users.map(user => (
+                                <tr key={user.id}>
+                                    <td className='manage-para'>{user.id}</td>
+                                    <td className='manage-para'>{user.email}</td>
+                                    <td className='manage-para'>{user.phone}</td>
+                                    <td className='manage-para'>{user.createdAt}</td>
+                                    <td className='manage-para'>{user.lastLoggedIn}</td>
+                                    <td>
+                                        {/* Add your action buttons here */}
+                                        <button className="edit-button-manage" onClick={() => handleEditButton(user)}>Edit</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
-<Modal
-    open={isModalOpen}
-    onClose={handleCloseModal}
-    style={{ width: "100%" }}
-  >
-    <Box
-      sx={{
-        position: "absolute",
-        width: "90%",
-        top: "50%",
-        left: "50%",
-        height: "500px",
-        overflow: "scroll",
-        transform: "translate(-50%, -50%)",
-        bgcolor: "background.paper",
-        borderRadius: "8px",
-        p: 3,
-      }}
-    >
-      <NewEditor
-        onClose={handleCloseModal}
-      />
-    </Box>
-  </Modal>
+            <Modal
+                open={isModalOpen}
+                onClose={handleCloseModal}
+                style={{ width: "100%" }}
+            >
+                <Box
+                    sx={{
+                        position: "absolute",
+                        width: "90%",
+                        top: "50%",
+                        left: "50%",
+                        height: "500px",
+                        overflow: "scroll",
+                        transform: "translate(-50%, -50%)",
+                        bgcolor: "background.paper",
+                        borderRadius: "8px",
+                        p: 3,
+                    }}
+                >
+                    <NewEditor
+                        onClose={handleCloseModal}
+                    />
+                </Box>
+            </Modal>
 
-  <Modal
-    open={editModel}
-    onClose={handleCloseModal}
-    style={{ width: "100%" }}
-  >
-    <Box
-      sx={{
-        position: "absolute",
-        width: "90%",
-        top: "50%",
-        left: "50%",
-        height: "500px",
-        overflow: "scroll",
-        transform: "translate(-50%, -50%)",
-        bgcolor: "background.paper",
-        borderRadius: "8px",
-        p: 3,
-      }}
-    >
-      <EditButton
-        onClose={handleCloseModal}
-      />
-    </Box>
-  </Modal>
+            <Modal
+                open={editModel}
+                onClose={handleCloseModal}
+                style={{ width: "100%" }}
+            >
+                <Box
+                    sx={{
+                        position: "absolute",
+                        width: "90%",
+                        top: "50%",
+                        left: "50%",
+                        height: "500px",
+                        overflow: "scroll",
+                        transform: "translate(-50%, -50%)",
+                        bgcolor: "background.paper",
+                        borderRadius: "8px",
+                        p: 3,
+                    }}
+                >
+                    {selectedUser && (
+                        <EditButton
+                            user={selectedUser}
+                            onClose={handleCloseModal}
+                        />
+                    )}
+                </Box>
+            </Modal>
 
-</>
-    )
-}
+        </>
+    );
+};
  
 export default Manageaccounts;
